2596b543ceebeb0a0179cbeeb67b2dfc15b0a951
LostViking09 <117819350+LostViking09@users.noreply.github.com>
2026-03-29 13:41:49 +0200

    Quartz sync: Mar 29, 2026, 1:41 PM

--- update_and_sync.ps1
+++ update_and_sync.ps1
@@ -13,7 +13,7 @@ if ($LASTEXITCODE -ge 8) {
 
 Write-Host "`n======================================================="
 Write-Host "Datumok automatikus javitasa (Letrehozas = Modositas)..."
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 10
 Get-ChildItem -File -Recurse | ForEach-Object { $_.CreationTime = $_.LastWriteTime }
 Write-Host "`n=======================================================`n"
 
